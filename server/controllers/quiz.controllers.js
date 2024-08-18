4cb38805e89864e9f4a77cbdb1ce4714cfe37ef7
Parfene Daniel <parfenedaniel345@gmail.com>
2024-08-19 00:54:13 +0300

    add quiz endpoints

--- server/controllers/quiz.controllers.js
+++ server/controllers/quiz.controllers.js
@@ -12,7 +12,11 @@ const createQuiz = async (req, res) => {
       ...req.body,
     };
     const newQuiz = new Quiz(quiz);
-    user.quizzes.push(newQuiz._id);
+    user.quizzes.push({
+      ID: newQuiz._id,
+      title: newQuiz.title,
+      category: newQuiz.category,
+    });
     await Promise.all([newQuiz.save(), user.save()]);
     res.status(201).json({ message: "Your quiz was created" });
   } catch (error) {
@@ -25,10 +29,6 @@ const createQuiz = async (req, res) => {
 const deleteQuiz = async (req, res) => {
   try {
     const quizId = req.params.quizId;
-    const isValidQuizId = mongoose.Types.ObjectId.isValid(quizId);
-    if (!isValidQuizId) {
-      return res.status(400).json({ message: "Your quiz id isn't valid" });
-    }
     const userId = req.user.id;
     const quiz = await Quiz.findById(quizId);
     if (!quiz) {
@@ -51,7 +51,27 @@ const deleteQuiz = async (req, res) => {
 
 const getQuiz = async (req, res) => {
   try {
-    return res.status(201).json({ message: "This is your quiz" });
+    const quizId = req.params.quizId;
+    const userId = req.user.id;
+    const quiz = await Quiz.findById(quizId);
+    if (!quiz) {
+      return res.status(404).json({ message: "This quiz doesn't exist" });
+    }
+    const user = await User.findById(userId);
+    if (quiz.userId == userId) {
+      res.json({ message: "This is your quiz", quiz, isYours: true });
+    } else {
+      const wasPlayed = user.played.find((id) => id == quizId) ? true : false;
+      const wasLiked = user.liked.find((id) => id == quizId) ? true : false;
+      res.json({
+        message: "This isn't your quiz",
+        quiz,
+        isYours: false,
+        played: wasPlayed,
+        liked: wasLiked,
+      });
+    }
+    return res.status(201);
   } catch (error) {
     res
       .status(500)
@@ -59,4 +79,77 @@ const getQuiz = async (req, res) => {
   }
 };
 
-module.exports = { createQuiz, deleteQuiz, getQuiz };
+const markAsPlayed = async (req, res) => {
+  try {
+    const { quizId } = req.body;
+    var { numberOfPoints } = req.body;
+    const userId = req.user.id;
+    const quiz = await Quiz.findById(quizId);
+    if (!quiz) {
+      return res.status(404).json({ message: "This quiz doesn't exist" });
+    }
+    if (quiz.userId == userId) {
+      return res
+        .status(403)
+        .json({ message: "This quiz is yours, you can't mark it" });
+    }
+    const user = await User.findById(userId);
+    const wasPlayed = user.played.find((id) => id == quizId);
+    if (wasPlayed) {
+      return res.status(201).json({ message: "You already played this quiz" });
+    }
+    if (numberOfPoints > quiz.questions.length) {
+      numberOfPoints = 0;
+    }
+    const authorQuiz = await User.findById(quiz.userId);
+    quiz.numberOfPlays = quiz.numberOfPlays + 1;
+    user.played.push(quizId);
+    user.points = user.points + numberOfPoints;
+    authorQuiz.numberOfPlays = authorQuiz.numberOfPlays + 1;
+    await Promise.all([quiz.save(), user.save(), authorQuiz.save()]);
+    res.status(201).json({ message: "This quiz was marked as played" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", errors: error.message });
+  }
+};
+
+const addLike = async (req, res) => {
+  try {
+    const { quizId } = req.body;
+    const userId = req.user.id;
+    const quiz = await Quiz.findById(quizId);
+    if (!quiz) {
+      return res.status(404).json({ message: "This quiz doesn't exist" });
+    }
+    if (quiz.userId == userId) {
+      return res
+        .status(403)
+        .json({ message: "This quiz is yours, you can't like it" });
+    }
+    const user = await User.findById(userId);
+    const wasLiked = user.liked.find((id) => id == quizId);
+    if (wasLiked) {
+      return res.status(201).json({ message: "You already liked this quiz" });
+    }
+    const authorQuiz = await User.findById(quiz.userId);
+    quiz.numberOfLikes = quiz.numberOfLikes + 1;
+    user.liked.push(quizId);
+    authorQuiz.numberOfLikes = authorQuiz.numberOfLikes + 1;
+    await Promise.all([quiz.save(), user.save(), authorQuiz.save()]);
+    res.status(201).json({ message: "This quiz was liked" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", errors: error.message });
+  }
+};
+
+module.exports = {
+  createQuiz,
+  deleteQuiz,
+  getQuiz,
+  markAsPlayed,
+  addLike,
+};
--- server/routes/quiz.route.js
+++ server/routes/quiz.route.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const authorization = require("../middlewares/authorization");
-const { createQuiz, deleteQuiz, getQuiz } = require("../controllers/quiz.controllers");
+const {
+  createQuiz,
+  deleteQuiz,
+  getQuiz,
+  markAsPlayed,
+  addLike,
+} = require("../controllers/quiz.controllers");
 
 const router = express.Router();
 
@@ -8,5 +14,7 @@ router.use(authorization);
 router.post("/createQuiz", createQuiz);
 router.delete("/deleteQuiz/:quizId", deleteQuiz);
 router.get("/getQuiz/:quizId", getQuiz);
+router.post("/markAsPlayed", markAsPlayed);
+router.post("/addLike", addLike);
 
 module.exports = router;
